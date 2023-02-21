2456143c469dc006ee985930b974e918c7d3c946
Ziyuan Han <hanziyuan08@outlook.com>
2023-02-21 15:57:36 +0800

    Update edufe.user.js

--- edufe.user.js
+++ edufe.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://training.sqkj.edufe.cn/*
+// @match        http://*.edufe.cn/*
 // @grant        none
 // ==/UserScript==
 
